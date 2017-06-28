96d87768ba430e71c90b81dd26c4ef69782b545e
Christopher Dumas <christopherdumas@Christophers-MacBook-Pro.local>
2017-06-28 15:48:08 -0700

    Coming not so soon

--- cpp/nouns/races.hpp
+++ cpp/nouns/races.hpp
@@ -1,12 +1,25 @@
 #include "../lib/utils.hpp"
+#include "../objects/items.hpp"
 #include <memory>
+#include <string>
+#include <vector>
 
 #pragma once
 namespace races
 {
     class Race
     {
-	
+    public:
+        int suggested_difficulty;
+        std::string name;
+        int bonus;
+        int speed;
+        int levels;
+        int color;
+        std::string description;
+        int max_health;
+        int strength;
+        std::vector<items::Item> starting_inventory;
     };
 
     const Race WARRIOR{};
